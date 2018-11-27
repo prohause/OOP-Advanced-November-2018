9a8222321ebc15cbfa94302bfbafc4bfbb54db9d
Gyunay Daud <prohause@live.com>
2018-11-27 13:31:45 +0200

    Problem 01 & 02 solved

---
+++ Exercise/Generics/P01_BoxOfString/Models/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace P01_BoxOfString.Models
+{
+    public class Box<T>
+    {
+        private readonly T _item;
+
+        public Box(T item)
+        {
+            _item = item;
+        }
+
+        public override string ToString()
+        {
+            return $"{_item.GetType()}: {_item.ToString()}";
+        }
+    }
+}
---
+++ Exercise/Generics/P01_BoxOfString/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace P01_BoxOfString
+{
+    using Models;
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            IList<Box<int>> myList = new List<Box<int>>();
+
+            var lineCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lineCount; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                myList.Add(new Box<int>(input));
+            }
+
+            foreach (var box in myList)
+            {
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
