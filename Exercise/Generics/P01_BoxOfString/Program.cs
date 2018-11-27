484f6f24e987a8450fe603ea05ce2330b5491576
Gyunay Daud <prohause@live.com>
2018-11-27 13:17:04 +0200

    Initial commit

---
+++ Exercise/Generics/P01_BoxOfString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01_BoxOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
